f7c503cc19a9f2a38cd9cb4849523fdb41f87fc0
ailtonbsj <ailton.ifce@gmail.com>
2026-05-02 18:08:05 -0300

    Add IFCE EBTT questions

--- src/app/features/practice/questions-list.ts
+++ src/app/features/practice/questions-list.ts
@@ -1,5 +1,9 @@
 export const questions = [
   {
+    name: 'IFCE 2026 Professor EBTT - Metodologia e Técnicas da Computação',
+    url: 'questions/IFCE 2026 Professor EBTT - Metodologia e Técnicas da Computação.json'
+  },
+  {
     name: 'Constituição Federal de 1988: Administração Pública (arts. 37 ao 41)',
     url: 'questions/1. Constituição Federal de 1988: Administração Pública (arts. 37 ao 41).json'
   }
